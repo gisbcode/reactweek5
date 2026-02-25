4aeee7a6ff628f7e7423b7bd1909598b1f231a97
Gigi <creative.giovannasb@gmail.com>
2026-02-25 14:32:16 +0000

    Set up first tolls for Weather app

--- src/index.js
+++ src/index.js
@@ -1,17 +1,36 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import Weather from "./Weather";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+      <footer>
+        This project was coded by
+        <a href="https://github.com/gisbcode" target="_blank" rel="noopener">
+          Giovanna Bellon
+        </a>
+        and is
+        <a
+          href="https://github.com/gisbcode/React-Weather-Forecast-Final-Project"
+          target="_blank"
+          rel="noopener"
+        >
+          on GitHub
+        </a>
+        and
+        <a
+          href="https://weatherforecastreactfinal.netlify.app/"
+          target="_blank"
+          rel="noopener"
+        >
+          Hosted on Netlify
+        </a>
+      </footer>
+    </div>
+  );
+}
